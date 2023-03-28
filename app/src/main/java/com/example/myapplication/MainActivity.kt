cb9eec004254c710a161e4b69b4d3172285a2372
Gopal Walia <k.gopal.2002@icloud.com>
2023-03-28 12:21:26 -0400

    done

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView by lazy {
+            findViewById(R.id.Timer)
+        }
+        val button: Button by lazy {
+            findViewById(R.id.button)
+        }
+
+        val scope = CoroutineScope(Job() + Dispatchers.Default)
+        button.setOnClickListener() {
+            scope.launch {
+                repeat(100) {
+                    (100-it).toString().run {
+                        Log.d("Countdown", this)
+                        withContext(Dispatchers.Main) {textView.text = this@run}
+                    }
+                    delay(1000)
+                }
+            }
+        }
+
     }
-}
